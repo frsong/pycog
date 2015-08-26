77e7d07be3fc3b6e31a877a5e15b1b175425ad69
frsong <song.francis@gmail.com>
2015-08-26 11:08:22 -0400

    Initial commit

--- paper/all.py
+++ paper/all.py
@@ -6,7 +6,7 @@ Note
 ----
 
 Everything means EVERYTHING, i.e., train, run trials, analyze, and make figures.
-This is mostly for us to check our work and for you to see what we did; 
+This is mostly for us to check our work and for you to see exactly what we did; 
 we don't recommend that you actually run this script.
 
 """
@@ -28,6 +28,40 @@ fmt  = a.format
 args = a.args
 
 #=========================================================================================
+# Shared steps
+#=========================================================================================
+
+def call(x):
+    subprocess.call(x, shell=True)
+
+def clean(model):
+    call("../examples/do.py models/{} clean".format(model))
+
+def train(model):
+    call("../examples/do.py models/{} train".format(model))
+
+def trials(model, analysis=None, ntrials=None):
+    if analysis is None:
+        analysis = model
+
+    if ntrials is None:
+        ntrials = ''
+    else:
+        ntrials = ' {}'.format(ntrials)
+        
+    call("../examples/do.py models/{} run analysis/{} trials{}"
+         .format(model, analysis, ntrials))
+
+def sort(model, analysis=None):
+    if analysis is None:
+        analysis = model
+
+    call("../examples/do.py models/{} run analysis/{} sort".format(model, analysis))
+
+def figure(figmaker):
+    call("../paper/{}.py".format(figmaker))
+
+#=========================================================================================
 
 if 'structure' in args:
     pass
@@ -42,4 +76,8 @@ if 'mante' in args:
     pass
 
 if 'lee' in args:
-    pass
+    clean('lee')
+    train('lee')
+    trials('lee')
+    sort('lee')
+    figure('fig_lee')
