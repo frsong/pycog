12caaae63204fb95f48d5997a0444da8676c5afa
frsong <song.francis@gmail.com>
2015-09-29 13:04:45 -0400

    Initial commit

--- examples/models/rdm_rt.py
+++ examples/models/rdm_rt.py
@@ -45,7 +45,7 @@ Cout[:,EXC] = 1
 cohs        = [1, 2, 4, 8, 16]
 in_outs     = [1, -1]
 nconditions = len(cohs)*len(in_outs)
-pcatch      = 2/(nconditions + 1)
+pcatch      = 1/(nconditions + 1)
 
 SCALE = 3.2
 def scale(coh):
