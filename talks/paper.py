a8db9034c0e20c2c4653a80531f1d903c5259214
frsong <song.francis@gmail.com>
2015-12-23 13:03:44 -0500

    Initial commit

--- talks/paper.py
+++
@@ -1,7 +0,0 @@
-import os
-from   os.path import join
-
-scratchroot   = os.environ.get('SCRATCH', join(os.path.expanduser('~'), 'scratch'))
-scratchpath   = join(scratchroot, 'work', 'examples')
-plotlabelsize = 11
-format        = 'pdf'
