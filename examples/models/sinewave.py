ce359d4b9fc187059e2079d7411eb9b15c386378
frsong <song.francis@gmail.com>
2016-01-20 12:13:29 -0500

    Added plotting to sine wave example

--- examples/models/sinewave.py
+++ examples/models/sinewave.py
@@ -69,3 +69,37 @@ def generate_trial(rng, dt, params):
 
 # Target error
 min_error = 0.05
+
+#/////////////////////////////////////////////////////////////////////////////////////////
+
+if __name__ == '__main__':
+    from pycog import Model
+
+    model = Model(N=N, Nout=Nout, ei=ei, tau=tau,
+                  train_brec=train_brec, train_bout=train_bout,
+                  var_rec=var_rec, generate_trial=generate_trial, min_error=min_error)
+    model.train('savefile.pkl')
+
+    #-------------------------------------------------------------------------------------
+    # Plot
+    #-------------------------------------------------------------------------------------
+
+    from pycog          import RNN
+    from pycog.figtools import Figure
+
+    rnn  = RNN('savefile.pkl', {'dt': 0.5})
+    info = rnn.run(T=10*period)
+
+    fig  = Figure()
+    plot = fig.add()
+
+    plot.plot(rnn.t/tau, rnn.z[0], color=Figure.colors('blue'))
+    plot.highlight(2*period, 3*period)
+    plot.text_upper_right('Range of training durations highlighted', dy=0.02)
+
+    plot.ylim(-1, 1)
+
+    plot.xlabel(r'$t/\tau$')
+    plot.ylabel('$\sin t$')
+
+    fig.save(path='.', name='sinewave')
